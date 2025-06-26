76e1b2cb843c0191c399ab8f6bebfbad1ddf0251
jaca230 <jaca230@uky.edu>
2025-06-26 01:14:48 -0400

    first commit

---
+++ include/stages/unpacking/base_midas_unpacker_stage.h
@@ -0,0 +1,32 @@
+#ifndef ANALYSIS_PIPELINE_STAGES_BASEMIDASUNPACKERSTAGE_H
+#define ANALYSIS_PIPELINE_STAGES_BASEMIDASUNPACKERSTAGE_H
+
+#include "stages/input/base_input_stage.h"
+#include "midas.h"
+#include "midasio.h"
+#include <any>
+
+class BaseMidasUnpackerStage : public BaseInputStage {
+public:
+    BaseMidasUnpackerStage();
+    ~BaseMidasUnpackerStage() override;
+
+    // Receives externally injected input (expects TMEvent inside std::any)
+    void SetInput(std::any input) override;
+
+    // Run unpacking on the most recent input
+    void Process() final override;
+
+protected:
+    // Called internally after casting input to TMEvent
+    void SetCurrentEvent(TMEvent& event);
+
+    const TMEvent* current_event_ = nullptr;
+
+    // Subclasses implement MIDAS unpacking logic here
+    virtual void ProcessMidasEvent(TMEvent& event) = 0;
+
+    ClassDefOverride(BaseMidasUnpackerStage, 1);
+};
+
+#endif // ANALYSIS_PIPELINE_STAGES_BASEMIDASUNPACKERSTAGE_H
---
+++ src/stages/unpacking/base_midas_unpacker_stage.cpp
@@ -0,0 +1,29 @@
+#include "stages/unpacking/base_midas_unpacker_stage.h"
+#include <stdexcept>
+#include <typeinfo>
+
+ClassImp(BaseMidasUnpackerStage)
+
+BaseMidasUnpackerStage::BaseMidasUnpackerStage() = default;
+BaseMidasUnpackerStage::~BaseMidasUnpackerStage() = default;
+
+void BaseMidasUnpackerStage::SetInput(std::any input) {
+    // Try to cast std::any to TMEvent reference
+    try {
+        TMEvent& event = std::any_cast<TMEvent&>(input);
+        SetCurrentEvent(event);
+    } catch (const std::bad_any_cast& e) {
+        throw std::runtime_error("BaseMidasUnpackerStage::SetInput - input is not a TMEvent reference");
+    }
+}
+
+void BaseMidasUnpackerStage::SetCurrentEvent(TMEvent& event) {
+    current_event_ = &event;
+}
+
+void BaseMidasUnpackerStage::Process() {
+    if (!current_event_) {
+        throw std::runtime_error("BaseMidasUnpackerStage: current_event_ not set");
+    }
+    ProcessMidasEvent(*const_cast<TMEvent*>(current_event_));
+}
